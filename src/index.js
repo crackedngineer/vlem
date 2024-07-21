1c9569b0949ff4014c3021a6c9924283efaca332
subhomoyrchoudhury@gmail.com <75subho2096>
2024-07-21 18:02:15 +0530

    fixes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
-import config from '../package.json' assert { type: 'json' };
+import config from "../package.json" assert { type: "json" };
 
-import chalk from 'chalk';
-import inquirer from 'inquirer';
-import { program } from 'commander';
+import chalk from "chalk";
+import inquirer from "inquirer";
+import { program } from "commander";
 
 import {
   rl,
@@ -13,9 +13,8 @@ import {
   ensureNetworkExists,
   runDockerContainer,
   fetchLabs,
-  listContainers,
-  getDockerDaemonPlatform
-} from "./utils.js"
+  listContainers
+} from "./utils.js";
 
 // Print the header
 function printHeader(githubLink) {
@@ -25,14 +24,17 @@ function printHeader(githubLink) {
   console.log(chalk.blue("*" + "-".repeat(terminalWidth - 2) + "*"));
   console.log(
     `${chalk.blue("*")}${chalk.yellow(
-      " ".repeat(Math.floor(padLength - 1)) + title + " ".repeat(Math.ceil(padLength - 1))
+      " ".repeat(Math.floor(padLength - 1)) +
+        title +
+        " ".repeat(Math.ceil(padLength - 1))
     )}${chalk.blue("*")}`
   );
   console.log(chalk.blue("*" + "-".repeat(terminalWidth - 2) + "*"));
   console.log(
     chalk.blue("*") +
-    chalk.white.bgMagenta(" GitHub: ") + chalk.black.bgWhite(` ${githubLink} `) +
-    chalk.blue("*")
+      chalk.white.bgMagenta(" GitHub: ") +
+      chalk.black.bgWhite(` ${githubLink} `) +
+      chalk.blue("*")
   );
   console.log(chalk.blue("*" + "-".repeat(terminalWidth - 2) + "*"));
 }
@@ -43,16 +45,18 @@ function printLabInfo(labObj) {
   console.log(chalk.greenBright("=".repeat(terminalWidth)));
   console.log(
     chalk.greenBright("Lab: ") +
-    chalk.white.green(` ${labObj.name} `.padEnd(terminalWidth - "Lab: ".length - 1, " "))
+      chalk.white.green(
+        ` ${labObj.name} `.padEnd(terminalWidth - "Lab: ".length - 1, " ")
+      )
   );
-  const words = labObj.description.split(' ');
+  const words = labObj.description.split(" ");
   let line = chalk.cyanBright("Description: ");
-  words.forEach(word => {
+  words.forEach((word) => {
     if (line.length + word.length + 1 > terminalWidth) {
       console.log(line);
       line = " ".repeat("Description: ".length);
     }
-    line += word + ' ';
+    line += word + " ";
   });
   if (line.trim().length > 0) {
     console.log(line);
@@ -64,31 +68,26 @@ function printLabInfo(labObj) {
 async function configureContainer(labObj) {
   const questions = [
     {
-      type: 'input',
-      name: 'containerName',
+      type: "input",
+      name: "containerName",
       message: "Enter container name",
       default: labObj.default.container_name, // Default value from labObj
     },
     {
-      type: 'input',
-      name: 'imageName',
+      type: "input",
+      name: "imageName",
       message: "Enter image name",
-      default: labObj.image // Default value from labObj
-
+      default: labObj.image, // Default value from labObj
     },
     {
-      type: 'input',
-      name: 'platform',
-      message: "Enter platform",
-      default: await getDockerDaemonPlatform() // Default value from labObj
+      type: "list", // Using 'list' for selection
+      name: "restartPolicy",
+      message: chalk.greenBright(
+        "Choose a restart policy for the Docker container"
+      ),
+      choices: ["no", "always", "unless-stopped", "on-failure"], // Options for restart policy
+      filter: (val) => val.toLowerCase(), // Ensuring the value is in lower case
     },
-    {
-      type: 'list', // Using 'list' for selection
-      name: 'restartPolicy',
-      message: chalk.greenBright("Choose a restart policy for the Docker container"),
-      choices: ['no', 'always', 'unless-stopped', 'on-failure'], // Options for restart policy
-      filter: (val) => val.toLowerCase() // Ensuring the value is in lower case
-    }
   ];
 
   const answers = await inquirer.prompt(questions);
@@ -99,7 +98,6 @@ async function configureContainer(labObj) {
   return {
     containerName: answers.containerName,
     imageName: answers.imageName,
-    platform: answers.platform,
     restartPolicy: answers.restartPolicy,
     portBinding: portBinding,
     applicationPort: applicationPort,
@@ -110,31 +108,36 @@ async function configureContainer(labObj) {
 async function initializeLab(labConfig, labObj) {
   await ensureNetworkExists(labObj.default.network);
   await runDockerContainer(
-    labConfig.containerName.startsWith("hlb-") ? labConfig.containerName : `hlb-${labConfig.containerName}`,
-    labConfig.platform,
+    labConfig.containerName.startsWith("hlb-")
+      ? labConfig.containerName
+      : `hlb-${labConfig.containerName}`,
     labObj.default.network,
     labConfig.restartPolicy,
     labConfig.portBinding,
     labConfig.imageName,
     labConfig.applicationPort,
+    labObj.default.environment_variables
   );
 }
 
 // Main function
 program
-  .name('hlb')
-  .description('CLI tool for for setting up the Local Penetration testing Environment')
+  .name("hlb")
+  .description(
+    "CLI tool for for setting up the Local Penetration testing Environment"
+  )
   .version(config.version);
 
-program.command("add")
+program
+  .command("add")
   .description("Add the lab environment")
-  .argument('<labName>', 'Name of the lab to fetch and manage')
+  .argument("<labName>", "Name of the lab to fetch and manage")
   .action(async (labName) => {
     try {
       const labs = await fetchLabs();
       const labObj = labs.find((item) => item.slug === labName);
       if (!labObj) {
-        throw new Error(`Cannot find a lab environment named ${labName}`)
+        throw new Error(`Cannot find a lab environment named ${labName}`);
       }
       const githubLink = "https://github.com/yourusername/bwapp-container";
 
@@ -143,15 +146,17 @@ program.command("add")
       printLabInfo(labObj);
 
       // Confirm if the user wants to proceed
-      const answer = await inquirer.prompt([{
-        type: 'confirm',
-        name: 'proceed',
-        message: 'Do you want to proceed with setting up this lab?',
-        default: false
-      }]);
+      const answer = await inquirer.prompt([
+        {
+          type: "confirm",
+          name: "proceed",
+          message: "Do you want to proceed with setting up this lab?",
+          default: false,
+        },
+      ]);
 
       if (!answer.proceed) {
-        console.log(chalk.yellow('Setup canceled by user.'));
+        console.log(chalk.yellow("Setup canceled by user."));
         return;
       }
 
@@ -163,15 +168,13 @@ program.command("add")
       rl.close();
     } catch (error) {
       console.error(chalk.redBright(error.message));
-      process.exit(0)
+      process.exit(0);
     }
   });
 
-program.command("labs").action(
-  async () => {
-    await listContainers('/hlb-')
-    process.exit(0)
-  }
-)
+program.command("labs").action(async () => {
+  await listContainers("/hlb-");
+  process.exit(0);
+});
 
 program.parse(process.argv);
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,6 @@
+import fs from "fs";
+import path from "path";
+
 import net from "net";
 import readline from "readline";
 
@@ -12,259 +15,255 @@ import { CONSTANTS } from "./constants.js";
 const docker = new Docker();
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
 const clearConsole = () => {
-    console.clear();
+  console.clear();
 };
 
 const checkPortAvailability = (port) => {
-    return new Promise((resolve, reject) => {
-        const server = net
-            .createServer()
-            .once("error", (err) => {
-                if (err.code === "EADDRINUSE") {
-                    resolve(false); // Port is in use
-                } else {
-                    reject(err); // Other errors
-                }
-            })
-            .once("listening", () => {
-                server.close(() => resolve(true)); // Port is available
-            })
-            .listen(port);
-    });
+  return new Promise((resolve, reject) => {
+    const server = net
+      .createServer()
+      .once("error", (err) => {
+        if (err.code === "EADDRINUSE") {
+          resolve(false); // Port is in use
+        } else {
+          reject(err); // Other errors
+        }
+      })
+      .once("listening", () => {
+        server.close(() => resolve(true)); // Port is available
+      })
+      .listen(port);
+  });
 };
 
 // Function to determine and assign the external port
 async function determineExternalPort(port, defaultPort) {
-    let externalPort =
-        port.type === CONSTANTS.PORT_TYPE.APPLICATION ? defaultPort : port.internal;
-    while (!(await checkPortAvailability(externalPort))) {
-        externalPort++;
-    }
-    return externalPort;
+  let externalPort =
+    port.type === CONSTANTS.PORT_TYPE.APPLICATION ? defaultPort : port.internal;
+  while (!(await checkPortAvailability(externalPort))) {
+    externalPort++;
+  }
+  return externalPort;
 }
 
 // Function to bind ports
 async function bindPorts(labObj) {
-    const portBinding = {};
-    const defaultPort = CONSTANTS.DEFAULT_PORT;
-    let applicationPort;
+  const portBinding = {};
+  const defaultPort = CONSTANTS.DEFAULT_PORT;
+  let applicationPort;
 
-    for (const port of labObj.default.ports) {
-        const externalPort = await determineExternalPort(port, defaultPort);
-        portBinding[externalPort] = port.internal;
+  for (const port of labObj.default.ports) {
+    const externalPort = await determineExternalPort(port, defaultPort);
+    portBinding[externalPort] = port.internal;
 
-        if (!applicationPort && port.type === CONSTANTS.PORT_TYPE.APPLICATION) {
-            applicationPort = externalPort;
-        }
+    if (!applicationPort && port.type === CONSTANTS.PORT_TYPE.APPLICATION) {
+      applicationPort = externalPort;
     }
+  }
 
-    return { portBinding, applicationPort };
+  return { portBinding, applicationPort };
 }
 
 const checkDockerInstalled = async () => {
-    try {
-        await docker.ping();
-    } catch (error) {
-        console.error(
-            chalk.red(
-                "Docker is not installed or not running. Please install Docker and run this script again."
-            )
-        );
-        process.exit(1);
-    }
+  try {
+    await docker.ping();
+  } catch (error) {
+    console.error(
+      chalk.red(
+        "Docker is not installed or not running. Please install Docker and run this script again."
+      )
+    );
+    process.exit(1);
+  }
 };
 
 const ensureNetworkExists = async (networkName) => {
-    try {
-        const networks = await docker.listNetworks();
-        const networkExists = networks.some(
-            (network) => network.Name === networkName
-        );
-        if (!networkExists) {
-            console.log(chalk.blue(`Creating network: `) + chalk.cyan(networkName));
-            await docker.createNetwork({ Name: networkName });
-        }
-    } catch (error) {
-        console.log(chalk.red("Failed to create network:"), error.message);
+  try {
+    const networks = await docker.listNetworks();
+    const networkExists = networks.some(
+      (network) => network.Name === networkName
+    );
+    if (!networkExists) {
+      console.log(chalk.blue(`Creating network: `) + chalk.cyan(networkName));
+      await docker.createNetwork({ Name: networkName });
     }
+  } catch (error) {
+    console.log(chalk.red("Failed to create network:"), error.message);
+  }
 };
 
 async function imageAvailable(imageName) {
-    try {
-        await docker.getImage(imageName).inspect();
-        return true;
-    } catch (error) {
-        return false;
-    }
+  try {
+    await docker.getImage(imageName).inspect();
+    return true;
+  } catch (error) {
+    return false;
+  }
 }
 
 async function pullImage(imageName, spinner) {
-    return new Promise((resolve, reject) => {
-        docker.pull(imageName, (err, stream) => {
-            if (err) {
-                spinner.fail("Error pulling image");
-                return reject(err);
-            }
-            docker.modem.followProgress(
-                stream,
-                (err, res) => (err ? reject(err) : resolve(res)),
-                (event) => {
-                    spinner.text = `Pulling image... ${event.status}`;
-                }
-            );
-        });
+  return new Promise((resolve, reject) => {
+    docker.pull(imageName, (err, stream) => {
+      if (err) {
+        spinner.fail("Error pulling image");
+        return reject(err);
+      }
+      docker.modem.followProgress(
+        stream,
+        (err, res) => (err ? reject(err) : resolve(res)),
+        (event) => {
+          spinner.text = `Pulling image... ${event.status}`;
+        }
+      );
     });
+  });
 }
 
 const runDockerContainer = async (
-    containerName,
-    platform,
-    network,
-    restartPolicy,
-    boundPorts,
-    imageName,
-    applicationPort
+  containerName,
+  network,
+  restartPolicy,
+  boundPorts,
+  imageName,
+  applicationPort,
+  environmentVariables
 ) => {
-    const spinner = ora("Starting Docker operations").start();
+  const spinner = ora("Starting Docker operations").start();
 
+  try {
+    // Remove existing container if it exists
+    const existingContainer = docker.getContainer(containerName);
     try {
-        // Remove existing container if it exists
-        const existingContainer = docker.getContainer(containerName);
-        try {
-            const containerInfo = await existingContainer.inspect(); // Check if container actually exists
-            if (containerInfo.State.Running) {
-                spinner.text = "Found existing container, stopping and removing...";
-                await existingContainer.stop();
-                await existingContainer.remove({ force: true });
-                spinner.succeed("Existing container stopped and removed");
-            } else {
-                spinner.text = "Found existing container, removing...";
-                await existingContainer.remove({ force: true });
-                spinner.succeed("Existing container removed");
-            }
-        } catch (err) {
-            if (err.statusCode === 404) {
-                // This error means the container does not exist, so no removal needed
-                spinner.text = "No active container found. Proceeding with creation...";
-            } else {
-                throw err;
-            }
-        }
-
-
-        // Check if image is available locally, otherwise pull it
-        spinner.text = `Checking for image ${imageName} locally...`;
-        const imageExists = await imageAvailable(imageName);
-        if (!imageExists) {
-            spinner.text = `Image ${imageName} not found locally. Pulling from Docker Hub...`;
-            await pullImage(imageName, spinner);
-        }
-
-        // Create and start the container
-        spinner.text = "Creating and starting the container";
-        const portBindings = Object.entries(boundPorts).reduce((acc, [hostPort, containerPort]) => {
-            acc[`${containerPort}/tcp`] = [{ HostPort: `${hostPort}` }];
-            return acc;
-        }, {});
-
-        const container = await docker.createContainer({
-            Image: imageName,
-            name: containerName,
-            Platform: platform,
-            // Env: Object.entries(environmentVariables).map(([key, value]) => `${key}=${value}`),
-            NetworkingConfig: {
-                EndpointsConfig: {
-                    [network]: {},
-                },
-            },
-            HostConfig: {
-                NetworkMode: network,
-                PortBindings: portBindings,
-                RestartPolicy: { Name: restartPolicy },
-            },
-        });
-        await container.start();
-        spinner.succeed(
-            `Container ${containerName} is deployed and running on port ${applicationPort}.`
-        );
-    } catch (error) {
-        spinner.fail("Error running Docker container");
-        console.log(chalk.red("Error:"), error.message);
+      const containerInfo = await existingContainer.inspect(); // Check if container actually exists
+      if (containerInfo.State.Running) {
+        spinner.text = "Found existing container, stopping and removing...";
+        await existingContainer.stop();
+        await existingContainer.remove({ force: true });
+        spinner.succeed("Existing container stopped and removed");
+      } else {
+        spinner.text = "Found existing container, removing...";
+        await existingContainer.remove({ force: true });
+        spinner.succeed("Existing container removed");
+      }
+    } catch (err) {
+      if (err.statusCode === 404) {
+        // This error means the container does not exist, so no removal needed
+        spinner.text = "No active container found. Proceeding with creation...";
+      } else {
+        throw err;
+      }
     }
-};
 
-
-const getDockerDaemonPlatform = async () => {
-    try {
-        const info = await docker.info();
-        const platform = info.OSType;
-        console.log(`Docker daemon platform: ${platform}`);
-        return platform;
-    } catch (error) {
-        console.error('Failed to get Docker platform information:', error);
-        throw error;
+    // Check if image is available locally, otherwise pull it
+    spinner.text = `Checking for image ${imageName} locally...`;
+    const imageExists = await imageAvailable(imageName);
+    if (!imageExists) {
+      spinner.text = `Image ${imageName} not found locally. Pulling from Docker Hub...`;
+      await pullImage(imageName, spinner);
     }
-};
 
+    // Create and start the container
+    spinner.text = "Creating and starting the container";
+    const portBindings = Object.entries(boundPorts).reduce(
+      (acc, [hostPort, containerPort]) => {
+        acc[`${containerPort}/tcp`] = [{ HostPort: `${hostPort}` }];
+        return acc;
+      },
+      {}
+    );
+
+    const container = await docker.createContainer({
+      Image: imageName,
+      name: containerName,
+      Env: Object.entries(environmentVariables).map(
+        ([key, value]) => `${key}=${value}`
+      ),
+      NetworkingConfig: {
+        EndpointsConfig: {
+          [network]: {},
+        },
+      },
+      HostConfig: {
+        NetworkMode: network,
+        PortBindings: portBindings,
+        RestartPolicy: { Name: restartPolicy },
+      },
+    });
+    await container.start();
+    spinner.succeed(
+      `Container ${containerName} is deployed and running on port ${applicationPort}.`
+    );
+  } catch (error) {
+    spinner.fail("Error running Docker container");
+    console.log(chalk.red("Error:"), error.message);
+  }
+};
 
 const fetchLabs = async () => {
+  if (process.env.HLB_DEBUG === "true") {
+    const filePath = "labs.json";
+    const jsonData = fs.readFileSync(filePath, "utf8");
+
+    const data = JSON.parse(jsonData);
+    return data;
+  } else {
     const url =
-        "https://raw.githubusercontent.com/Hacker-Lab-Pro/hackerlab-scripts/main/labs.json";
+      "https://raw.githubusercontent.com/Hacker-Lab-Pro/hackerlab-scripts/main/labs.json";
     try {
-        const response = await axios.get(url);
-        const labs = response.data;
-        return labs;
+      const response = await axios.get(url);
+      const labs = response.data;
+      return labs;
     } catch (err) {
-        console.log(chalk.red("Error fetching the data:"), err.message);
-        return [];
+      console.log(chalk.red("Error fetching the data:"), err.message);
+      return [];
     }
+  }
 };
 
 const listContainers = async (suffix) => {
-    try {
-        const containers = await docker.listContainers({ all: true });
-        const hlbContainers = containers.filter((container) =>
-            container.Names.some((name) => name.startsWith(suffix))
-        );
+  try {
+    const containers = await docker.listContainers({ all: true });
+    const hlbContainers = containers.filter((container) =>
+      container.Names.some((name) => name.startsWith(suffix))
+    );
 
-        if (hlbContainers.length === 0) {
-            console.log(chalk.yellow('No containers found starting with "hlb-"'));
-            return;
-        }
-
-        console.log(chalk.green("Lab Environments:\n"));
-        hlbContainers.forEach((container) => {
-            console.log(
-                chalk.blue(
-                    `Container Name: ${container.Names.join(", ").replace(/^\//, "")}`
-                )
-            ); // Removes leading slash
-            // console.log(chalk.magenta(`Container ID: ${container.Id}`));
-            console.log(chalk.cyan(`Image: ${container.Image}`));
-            console.log(chalk.cyan(`Status: ${container.Status}`));
-            console.log(chalk.cyan(`State: ${container.State}`)); // Running, Paused, etc.
-            console.log(
-                chalk.gray(`\n--------------------------------------------------`)
-            );
-        });
-    } catch (error) {
-        console.log(chalk.red(`Error fetching containers: ${error.message}`));
+    if (hlbContainers.length === 0) {
+      console.log(chalk.yellow('No containers found starting with "hlb-"'));
+      return;
     }
+
+    console.log(chalk.green("Lab Environments:\n"));
+    hlbContainers.forEach((container) => {
+      console.log(
+        chalk.blue(
+          `Container Name: ${container.Names.join(", ").replace(/^\//, "")}`
+        )
+      ); // Removes leading slash
+      // console.log(chalk.magenta(`Container ID: ${container.Id}`));
+      console.log(chalk.cyan(`Image: ${container.Image}`));
+      console.log(chalk.cyan(`Status: ${container.Status}`));
+      console.log(chalk.cyan(`State: ${container.State}`)); // Running, Paused, etc.
+      console.log(
+        chalk.gray(`\n--------------------------------------------------`)
+      );
+    });
+  } catch (error) {
+    console.log(chalk.red(`Error fetching containers: ${error.message}`));
+  }
 };
 
 export {
-    rl,
-    clearConsole,
-    bindPorts,
-    checkDockerInstalled,
-    ensureNetworkExists,
-    runDockerContainer,
-    fetchLabs,
-    listContainers,
-    getDockerDaemonPlatform
+  rl,
+  clearConsole,
+  bindPorts,
+  checkDockerInstalled,
+  ensureNetworkExists,
+  runDockerContainer,
+  fetchLabs,
+  listContainers
 };
