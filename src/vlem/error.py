22dc05e930bef2f439b86cbab5e063415a3a9bb0
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2025-05-18 22:39:47 +0530

    added logger in utils

--- src/vlem/error.py
+++ src/vlem/error.py
@@ -1,6 +1,9 @@
 class LabNotFoundError(Exception):
+    """Custom exception for when a lab is not found."""
+
     def __init__(self, lab_name: str):
-        super().__init__(f"Lab with name '{lab_name}' not found.")
+        super().__init__(f"Lab not found: {lab_name}")
+        self.lab_name = lab_name
 
 
 class PullImageError(Exception):
